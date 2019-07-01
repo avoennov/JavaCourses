345780dfa2e57e662d0da72e0865cbc345ca2b79
avoennov <52177854+avoennov@users.noreply.github.com>
2019-07-01 21:26:48 +0300

    Lesson#2. Added case with method

--- sandbox/src/main/java/lessons/les2/GetDistance.java
+++ sandbox/src/main/java/lessons/les2/GetDistance.java
@@ -7,6 +7,7 @@ public class GetDistance {
         Point p1 = new Point(1,2);
         Point p2 = new Point(6,2);
 
-        System.out.println("Расстояние между точками P1 и P2 составляет: " + Point.distance(p1, p2));
+        System.out.println("Функция. Расстояние между точками P1 и P2 составляет: " + Point.distance(p1, p2));
+        System.out.println("Метод. Расстояние между точками P1 и P2 составляет: " + p1.distanceMethod(p2));
     }
 }
