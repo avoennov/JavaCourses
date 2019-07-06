97013c91b352a16664361848e7395e17cf0013e7
avoennov <52177854+avoennov@users.noreply.github.com>
2019-07-07 00:42:58 +0300

    Addressbook. Home work #4. Remove excess actions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -21,48 +21,20 @@ public class ContactCreationTests {
   public void testContactCreationTests() throws Exception {
 
     wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
     wd.findElement(By.name("firstname")).sendKeys("Ivan");
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
     wd.findElement(By.name("middlename")).sendKeys("Ivanovich");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
     wd.findElement(By.name("lastname")).sendKeys("Ivanov");
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
     wd.findElement(By.name("nickname")).sendKeys("AgentSmith");
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
     wd.findElement(By.name("title")).sendKeys("Some Title");
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
     wd.findElement(By.name("company")).sendKeys("The Matrix");
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
     wd.findElement(By.name("address")).sendKeys("K. Marksa street, 18");
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
     wd.findElement(By.name("home")).sendKeys("01-123456-07");
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
     wd.findElement(By.name("mobile")).sendKeys("02-11-223-01");
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
     wd.findElement(By.name("work")).sendKeys("14-256-01-28");
-    wd.findElement(By.name("fax")).click();
-    wd.findElement(By.name("fax")).clear();
     wd.findElement(By.name("fax")).sendKeys("214-15-78-268");
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
     wd.findElement(By.name("email")).sendKeys("test@mail.com");
-    wd.findElement(By.name("email2")).clear();
     wd.findElement(By.name("email2")).sendKeys("test2@mail.com");
-    wd.findElement(By.name("email3")).clear();
     wd.findElement(By.name("email3")).sendKeys("test3@mail.com");
-    wd.findElement(By.name("homepage")).click();
-    wd.findElement(By.name("homepage")).clear();
     wd.findElement(By.name("homepage")).sendKeys("localhost");
     wd.findElement(By.name("bday")).click();
     new Select(wd.findElement(By.name("bday"))).selectByVisibleText("5");
@@ -70,8 +42,6 @@ public class ContactCreationTests {
     wd.findElement(By.name("bmonth")).click();
     new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("March");
     wd.findElement(By.name("bmonth")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
     wd.findElement(By.name("byear")).sendKeys("2047");
     wd.findElement(By.name("aday")).click();
     new Select(wd.findElement(By.name("aday"))).selectByVisibleText("6");
@@ -82,18 +52,12 @@ public class ContactCreationTests {
     wd.findElement(By.name("ayear")).click();
     wd.findElement(By.name("ayear")).clear();
     wd.findElement(By.name("ayear")).sendKeys("2017");
+/*    wd.findElement(By.name("new_group")).click();
+    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText("test1");*/
     wd.findElement(By.name("new_group")).click();
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText("test1");
-    wd.findElement(By.name("new_group")).click();
-    wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
     wd.findElement(By.name("address2")).sendKeys("Lenina street, 35");
     wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("notes")).click();
-    wd.findElement(By.name("notes")).clear();
     wd.findElement(By.name("notes")).sendKeys("Some notes");
-    wd.findElement(By.name("phone2")).click();
-    wd.findElement(By.name("phone2")).clear();
     wd.findElement(By.name("phone2")).sendKeys("35-18-29");
     wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
     wd.findElement(By.linkText("home page")).click();
@@ -105,11 +69,7 @@ public class ContactCreationTests {
   }
 
   private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
