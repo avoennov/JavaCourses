e7981db28a4bd5ed141eeaaab6091b1e403936f6
avoennov <52177854+avoennov@users.noreply.github.com>
2019-07-07 01:14:23 +0300

    Addressbook. Home work #4. Add helper class. Remove excess actions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -20,50 +20,40 @@ public class ContactCreationTests {
   @Test
   public void testContactCreationTests() throws Exception {
     gotoContactPage();
-    fillContactForm("Ivan", "Ivanovich", "Ivanov", "AgentSmith", "Some Title", "The Matrix", "K. Marksa street, 18", "01-123456-07", "02-11-223-01", "14-256-01-28", "214-15-78-268", "test@mail.com", "test2@mail.com", "test3@mail.com", "localhost", "5", "March", "2047", "6", "May", "2017", "Lenina street, 35", "Some notes", "35-18-29");
+    fillContactForm(new ContactData("Ivanovich", "Ivanov", "AgentSmith", "Some Title", "The Matrix", "K. Marksa street, 18", "01-123456-07", "02-11-223-01", "14-256-01-28", "214-15-78-268", "test@mail.com", "test2@mail.com", "test3@mail.com", "localhost", "5", "March", "2047", "Ivan", "6", "May", "2017", "Lenina street, 35", "Some notes", "35-18-29"));
     submitContactCreation();
     returnToHomePage();
   }
 
-  private void fillContactForm(String firstname, String middlename, String lastname, String nickname, String title, String company, String address, String home, String mobile, String work, String fax, String email, String email2, String email3, String homepage, String bday, String bmonth, String byear, String aday, String amonth, String ayear, String address2, String notes, String phone2) {
-    wd.findElement(By.name("firstname")).sendKeys(firstname);
-    wd.findElement(By.name("middlename")).sendKeys(middlename);
-    wd.findElement(By.name("lastname")).sendKeys(lastname);
-    wd.findElement(By.name("nickname")).sendKeys(nickname);
-    wd.findElement(By.name("title")).sendKeys(title);
-    wd.findElement(By.name("company")).sendKeys(company);
-    wd.findElement(By.name("address")).sendKeys(address);
-    wd.findElement(By.name("home")).sendKeys(home);
-    wd.findElement(By.name("mobile")).sendKeys(mobile);
-    wd.findElement(By.name("work")).sendKeys(work);
-    wd.findElement(By.name("fax")).sendKeys(fax);
-    wd.findElement(By.name("email")).sendKeys(email);
-    wd.findElement(By.name("email2")).sendKeys(email2);
-    wd.findElement(By.name("email3")).sendKeys(email3);
-    wd.findElement(By.name("homepage")).sendKeys(homepage);
+  private void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).sendKeys(contactData.getHome());
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    wd.findElement(By.name("work")).sendKeys(contactData.getWork());
+    wd.findElement(By.name("fax")).sendKeys(contactData.getFax());
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
+    wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
+    wd.findElement(By.name("homepage")).sendKeys(contactData.getHomepage());
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(bday);
-    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(bmonth);
-    wd.findElement(By.name("bmonth")).click();
-    wd.findElement(By.name("byear")).sendKeys(byear);
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
+    wd.findElement(By.name("byear")).sendKeys(contactData.getByear());
     wd.findElement(By.name("aday")).click();
-    new Select(wd.findElement(By.name("aday"))).selectByVisibleText(aday);
-    wd.findElement(By.name("aday")).click();
+    new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactData.getAday());
     wd.findElement(By.name("amonth")).click();
-    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(amonth);
-    wd.findElement(By.name("amonth")).click();
-    wd.findElement(By.name("ayear")).click();
-    wd.findElement(By.name("ayear")).clear();
-    wd.findElement(By.name("ayear")).sendKeys(ayear);
-/*    wd.findElement(By.name("new_group")).click();
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText("test1");*/
-    wd.findElement(By.name("new_group")).click();
-    wd.findElement(By.name("address2")).sendKeys(address2);
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("notes")).sendKeys(notes);
-    wd.findElement(By.name("phone2")).sendKeys(phone2);
+    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactData.getAmonth());
+    wd.findElement(By.name("ayear")).sendKeys(contactData.getAyear());
+    wd.findElement(By.name("address2")).sendKeys(contactData.getAddress2());
+    wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
+    wd.findElement(By.name("phone2")).sendKeys(contactData.getPhone2());
   }
 
   private void returnToHomePage() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,151 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String home;
+    private final String mobile;
+    private final String work;
+    private final String fax;
+    private final String email;
+    private final String email2;
+    private final String email3;
+    private final String homepage;
+    private final String bday;
+    private final String bmonth;
+    private final String byear;
+    private final String firstname;
+    private final String aday;
+    private final String amonth;
+    private final String ayear;
+    private final String address2;
+    private final String notes;
+    private final String phone2;
+
+    public ContactData(String middlename, String lastname, String nickname, String title, String company, String address, String home, String mobile, String work, String fax, String email, String email2, String email3, String homepage, String bday, String bmonth, String byear, String firstname, String aday, String amonth, String ayear, String address2, String notes, String phone2) {
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.home = home;
+        this.mobile = mobile;
+        this.work = work;
+        this.fax = fax;
+        this.email = email;
+        this.email2 = email2;
+        this.email3 = email3;
+        this.homepage = homepage;
+        this.bday = bday;
+        this.bmonth = bmonth;
+        this.byear = byear;
+        this.firstname = firstname;
+        this.aday = aday;
+        this.amonth = amonth;
+        this.ayear = ayear;
+        this.address2 = address2;
+        this.notes = notes;
+        this.phone2 = phone2;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public String getBmonth() {
+        return bmonth;
+    }
+
+    public String getByear() {
+        return byear;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getAday() {
+        return aday;
+    }
+
+    public String getAmonth() {
+        return amonth;
+    }
+
+    public String getAyear() {
+        return ayear;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+}
