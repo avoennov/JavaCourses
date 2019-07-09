3d02c00feaa12c0518a401d3374c31779cb01abd
avoennov <52177854+avoennov@users.noreply.github.com>
2019-07-09 23:43:38 +0300

    Addressbook. Create basic class for helper classes. Less2.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -25,8 +23,6 @@ public class ApplicationManager {
         sessionHelper.login("admin", "secret");
     }
 
-
-
     private void logout() {
       wd.findElement(By.linkText("Logout")).click();
     }
@@ -34,24 +30,6 @@ public class ApplicationManager {
     public void stop() {
         logout();
         wd.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-      try {
-        wd.findElement(by);
-        return true;
-      } catch (NoSuchElementException e) {
-        return false;
-      }
-    }
-
-    private boolean isAlertPresent() {
-      try {
-        wd.switchTo().alert();
-        return true;
-      } catch (NoAlertPresentException e) {
-        return false;
-      }
     }
 
     public GroupHelper getGroupHelper() {
