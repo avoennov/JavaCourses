147fa975386adc754bd1ed158f7fd7f84946e199
avoennov <52177854+avoennov@users.noreply.github.com>
2019-08-05 23:40:18 +0300

    Addressbook. Less 5.9 - 5.10 Ways to build complex locators. Cut the lines

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -74,7 +74,7 @@ public class ContactHelper extends HelperBase{
     }
 
     public void openEditor(int id) {
-      wd.findElement(By.cssSelector("a[href='" + "edit.php?id=" + id + "']")).click();
+      wd.findElement(By.cssSelector(String.format("a[href='edit.php?id=%s']", id))).click();
     }
 
     public void initContactCreation() {
@@ -116,8 +116,21 @@ public class ContactHelper extends HelperBase{
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             String lastname = element.findElement(By.xpath("./td[2]")).getText();
             String firstname = element.findElement(By.xpath("./td[3]")).getText();
-            contacts.add(new ContactData().withId(id).withLastname(lastname).withFirstname(firstname));
+            String[] phones = element.findElement(By.xpath("./td[6]")).getText().split("\n");
+            contacts.add(new ContactData().withId(id).withLastname(lastname).withFirstname(firstname).withHome(phones[0])
+                    .withMobile(phones[1]).withWork(phones[2]));
         }
         return contacts;
+    }
+
+    public ContactData infoFromEditForm(ContactData contact) {
+        openEditor(contact.getId());
+        String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
+        String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+        String home = wd.findElement(By.name("home")).getAttribute("value");
+        String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
+        String work = wd.findElement(By.name("work")).getAttribute("value");
+        wd.navigate().back();
+        return new ContactData().withId(contact.getId()).withFirstname(firstname).withLastname(lastname).withHome(home).withMobile(mobile).withWork(work);
     }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactPhoneTests extends TestBase {
+
+    @Test(enabled = true)
+    public void testContactPhones() {
+        app.goTo().homePage();
+        ContactData contact = app.contact().all().iterator().next();
+        ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+        assertThat(contact.getHome(), equalTo(cleaned(contactInfoFromEditForm.getHome())));
+        assertThat(contact.getMobile(), equalTo(cleaned(contactInfoFromEditForm.getMobile())));
+        assertThat(contact.getWork(), equalTo(cleaned(contactInfoFromEditForm.getWork())));
+    }
+
+    public String cleaned(String phone) {
+        return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
+    }
+}
