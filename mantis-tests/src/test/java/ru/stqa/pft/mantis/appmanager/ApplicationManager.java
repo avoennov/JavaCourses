71835445104711b86487f39be33b40be65f8b9d8
avoennov <52177854+avoennov@users.noreply.github.com>
2019-08-21 01:41:18 +0300

    Mantis. Less 8.3 Direct interaction with the server via HTTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -43,4 +43,12 @@ public class ApplicationManager {
         //sessionHelper.logout();
         wd.quit();
     }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
