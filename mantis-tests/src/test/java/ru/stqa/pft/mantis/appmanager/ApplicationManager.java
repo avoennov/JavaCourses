78773412acd860c3edc1dfd02fe7c0577f8f90d6
avoennov <52177854+avoennov@users.noreply.github.com>
2019-08-27 15:16:07 +0300

    SOAP. Less 9.3 Interaction with MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
     private MailHelper mailHelper;
     private JamesHelper jamesHelper;
     private DbHelper dbHelper;
+    private SoapHelper soapHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -93,5 +94,12 @@ public class ApplicationManager {
     public DbHelper db(){
         dbHelper = new DbHelper();
         return dbHelper;
+    }
+
+    public SoapHelper soap() {
+        if(soapHelper == null) {
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
 }
