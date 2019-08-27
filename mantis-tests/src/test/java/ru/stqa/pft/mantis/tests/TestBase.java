97db92f3d4c8d4f0fa30b9169ca57a73cbb7fdcc
avoennov <52177854+avoennov@users.noreply.github.com>
2019-08-27 22:19:14 +0300

    Mantis. Home work #19 Integration with the MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
+import javax.xml.rpc.ServiceException;
+
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+import java.math.BigInteger;
 
 public class TestBase {
 
@@ -22,5 +30,22 @@ public class TestBase {
     public void tearDown() throws Exception {
         app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
+    }
+
+    public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
+    }
+
+    private boolean isIssueOpen (int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mc = app.soap().getMantisConnect();
+        IssueData issueData = mc.mc_issue_get("administrator", "root", BigInteger.valueOf(issueId));
+        String resolution = String.valueOf(issueData.getResolution().getName());
+        System.out.println(resolution);
+        if (resolution.equals("open")){
+            return true;
+        }
+        return false;
     }
 }
